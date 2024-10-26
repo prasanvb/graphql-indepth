e46bc1917d92816c86bebc6c3419a2422a918b1b
prasanv <prasan.venkatb@gmail.com>
2024-10-26 16:28:59 -0700

    frontend  code

--- job-board-project/server/server.js
+++ job-board-project/server/server.js
@@ -13,11 +13,13 @@ app.use(cors(), express.json(), authMiddleware);
 
 app.post('/login', handleLogin);
 
-const typeDefs = await readFile('./schema.graphql', 'utf-8');
-const apolloServer = new ApolloServer({ typeDefs, resolvers });
-await apolloServer.start();
+(async () => {
+  const typeDefs = await readFile('./schema.graphql', 'utf-8');
+  const apolloServer = new ApolloServer({ typeDefs, resolvers });
 
-app.use('/graphql', apolloExpressMiddleware(apolloServer));
+  await apolloServer.start();
+  app.use('/graphql', apolloExpressMiddleware(apolloServer));
+})();
 
 app.listen({ port: PORT }, () => {
   console.log(`Server running on port ${PORT}`);
