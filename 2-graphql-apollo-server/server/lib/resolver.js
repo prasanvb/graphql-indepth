9bae2bff23790aa0ab6f14912fe5b991c71e829f
prasanv <prasan.venkatb@gmail.com>
2024-11-03 19:19:58 -0800

    refactor: project-2-server

--- 2-graphql-apollo-server/server/lib/resolver.js
+++ 2-graphql-apollo-server/server/lib/resolver.js
@@ -19,11 +19,7 @@ export const resolvers = {
   Query: {
     // NOTE: fetchJobs has 2 layer resolver functions 1.getJobs() and 2.Job
     // Data returned by getJobs() is used as input inside Job function
-    fetchJobs: (_root, args) => {
-      // NOTE: limit and offset args are used for pagination
-      const { limit, offset } = args;
-      return getJobs(limit, offset);
-    },
+    fetchJobs: () => getJobs(),
     // NOTE: args is object with query parameter value
     fetchJob: async (_root, args) => {
       const job = await getJob(args.id);
--- 2-graphql-apollo-server/server/utils/jobs.js
+++ 2-graphql-apollo-server/server/utils/jobs.js
@@ -3,17 +3,8 @@ import { generateId } from './ids.js';
 
 const getJobTable = () => connection.table('job');
 
-export async function getJobs(limit, offset) {
+export async function getJobs() {
   const query = getJobTable().select().orderBy('createdAt', 'desc');
-
-  if (limit) {
-    query.limit(limit);
-  }
-
-  if (offset) {
-    query.offset(offset);
-  }
-
   return await query;
 }
 
